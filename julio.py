26aebed56b17ab76f46037ec65d2f2508cf1c30a
Millena Ferreira dos Reis <millena.ferreira@gmail.com>
2020-04-18 19:30:56 -0300

    julio.py
    inclusao da requisicao na pagina do desafio para ler o json e decriptar a mensagem dele

--- julio.py
+++ julio.py
@@ -1,19 +1,22 @@
 
-#dados = '{"dado1": 1, "dado2": "nome 2", "dado3": "descricao 3"}'
 
+import requests
+import json
+
+# requisita o json na url para fazer a conversao
+urlToken="https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=89239c926b23ba1e2545d15574e745b3a1f9539a"
+
+r = requests.get(urlToken)
+dadosRecebidos = r.json()
+
+
+# de-para da criptografia
 listaAlfaOrig = [" ",",",".",";",":","!","?","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w"]
 listaAlfaCifr = [" ",",",".",";",":","!","?","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 
-dadosRecebidos = {"numero_casas":5,"token":"89239c926b23ba1e2545d15574e745b3a1f9539a","cifrado":"n tsqd gjqnjaj ns xyfynxynhx ymfy n ithytwji rdxjqk. bnsxyts x. hmzwhmnqq?","decifrado":"","resumo_criptografico":""}
+#dadosRecebidos = {"numero_casas":5,"token":"89239c926b23ba1e2545d15574e745b3a1f9539a","cifrado":"n tsqd gjqnjaj ns xyfynxynhx ymfy n ithytwji rdxjqk. bnsxyts x. hmzwhmnqq?","decifrado":"","resumo_criptografico":""}
 
-"""
-dados = aspas + dadosRecebidos + aspas
-dadosJson = json.loads(dados)
-print(type(dadosJson))
-print(dadosJson)
-print(dadosJson["cifrado"])
-"""
 
 frase = dadosRecebidos["cifrado"]
 frase = frase.lower()
@@ -23,6 +26,7 @@ print(frase)
 
 tamanho = len(frase)
 
+# traducao da mensagem
 for i in range(0, len(frase)):
     
     for j in range(0, len(listaAlfaCifr)):
@@ -33,9 +37,14 @@ for i in range(0, len(frase)):
 print(tamanho)
 print(fraseTraduzida)
 
+#inclusao do texto decifrado no json para saida
 dadosRecebidos["decifrado"] = fraseTraduzida
 
 print(dadosRecebidos)
+
+
+
+
 
 
 
